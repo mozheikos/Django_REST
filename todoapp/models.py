1dd05a1bc1ddb2917cc56fa682b6e7057d642b0a
mozheikos <mozheiko.stanislav@yandex.ru>
2022-03-22 22:37:23 +0300

    lesson 3 task 2: Project model, migrations

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from usersapp.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    repository_link = models.URLField()
+    users = models.ManyToManyField(User)
