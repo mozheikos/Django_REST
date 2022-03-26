9132ebe94ea973e6d95c05dbd48266802fa2b6f6
mozheikos <mozheiko.stanislav@yandex.ru>
2022-03-26 09:21:44 +0300

    lesson 4: UserModelViewSet -> CustomViewSet, without deletion and creation

--- usersapp/views.py
+++ usersapp/views.py
@@ -1,11 +1,12 @@
 from .models import User
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import  GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from django.http import JsonResponse
 
 from .serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
 
