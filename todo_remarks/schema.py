77bbee51926aca4d479f27e568bf7e25e66391c6
mozheikos <mozheiko.stanislav@yandex.ru>
2022-04-14 23:32:39 +0300

    first part

--- todo_remarks/schema.py
+++ todo_remarks/schema.py
@@ -1,4 +1,6 @@
-from graphene import Schema, String, List, ObjectType, Mutation, ID
+import typing
+
+from graphene import Schema, String, List, ObjectType, Mutation, ID, Field, Int
 from graphene_django import DjangoObjectType
 from usersapp.models import User
 from todoapp.models import Project, ToDo
@@ -32,17 +34,51 @@ class Query(ObjectType):
     all_projects = List(ProjectType)
     all_remarks = List(TODOType)
 
+    # one
+    me = Field(UserType)
+    user_by_id = Field(UserType, pk=Int(required=False))
+    project_by_id = Field(ProjectType, pk=Int(required=False))
+    remark_by_id = Field(TODOType, pk=Int(required=False))
+
+    # Users list
     @staticmethod
-    def resolve_all_users(root, info):
+    def resolve_all_users(root, info) -> typing.List[User]:
         return User.objects.all()
 
+    # Projects list
     @staticmethod
-    def resolve_all_projects(root, info):
+    def resolve_all_projects(root, info) -> typing.List[Project]:
         return Project.objects.all()
 
+    # Remarks list
     @staticmethod
-    def resolve_all_remarks(root, info):
+    def resolve_all_remarks(root, info) -> typing.List[ToDo]:
         return ToDo.objects.all()
+
+    # get current authorized user
+    @staticmethod
+    def resolve_me(root, info) -> typing.Union[User, None]:
+        user = info.context.user
+        return user if user else None
+
+    # get user by id
+    @staticmethod
+    def resolve_user_by_id(root, info, pk: int = None) -> typing.Union[User, None]:
+        if pk:
+            user = User.objects.filter(pk=pk).first()
+            return user if user else None
+        return None
+
+    # get project by id (if authorized)
+    @staticmethod
+    def resolve_project_by_id(root, info, pk: int = None) -> typing.Union[Project, None]:
+        # added this check because of permission settings for projects
+        if info.context.user.is_authenticated:
+            if pk:
+                project = Project.objects.filter(pk=pk).first()
+                return project if project else None
+            return None
+        return None
 
 
 schema = Schema(query=Query)
